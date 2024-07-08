5c99471c507141b75b86e9a117ecd0d4763435b4
YaqutVerdiyeva <yaqutverdiyeva22@gmail.com>
2024-07-08 18:47:04 +0400

    responsive navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,10 @@ let openMenu = document.querySelector(".open-menu");
 let closeMenu = document.querySelector(".x-logo");
 let sideBar = document.querySelector(".sidebar");
 let sideBarContent = document.querySelector(".sidebar-content");
+let responsiveOpenBtn = document.querySelector(".menu-logo");
+let responsiveMenu = document.querySelector(".responsive-menu");
+let responsiveContent = document.querySelector(".responsive-content");
+let closeResponsiveMenu = document.querySelector(".close-responsive-menu");
 openMenu.addEventListener("click", () => {
   sideBar.style.width = "540px";
   sideBar.style.padding = " 0 60px";
@@ -11,4 +15,14 @@ closeMenu.addEventListener("click", () => {
   sideBar.style.width = "0px";
   sideBar.style.padding = " 0";
   sideBarContent.style.display = "none";
+});
+responsiveOpenBtn.addEventListener("click", () => {
+  responsiveContent.style.display = "block";
+  responsiveMenu.style.padding = "10px 40px";
+  responsiveMenu.style.width = "100%";
+});
+closeResponsiveMenu.addEventListener("click", () => {
+  responsiveContent.style.display = "none";
+  responsiveMenu.style.padding = "0";
+  responsiveMenu.style.width = "0%";
 });
