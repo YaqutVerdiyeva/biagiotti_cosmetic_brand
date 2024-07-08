188a4fda81719e5fea47638218c169e03d603e60
YaqutVerdiyeva <yaqutverdiyeva22@gmail.com>
2024-07-09 00:25:25 +0400

    some changes

---
+++ assets/js/home.js
@@ -0,0 +1,8 @@
+let star = document.querySelectorAll(".fa-star");
+
+star.forEach((el) => {
+  el.addEventListener("click", () => {
+    console.log("jhdfbc");
+    el.style.color = "yellow";
+  });
+});
