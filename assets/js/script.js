8b4febc753009d0595104f3ff9d8e6758b3d91d5
YaqutVerdiyeva <yaqutverdiyeva22@gmail.com>
2024-07-09 21:25:01 +0400

    completed home page and added footer in all pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,10 @@ let responsiveOpenBtn = document.querySelector(".menu-logo");
 let responsiveMenu = document.querySelector(".responsive-menu");
 let responsiveContent = document.querySelector(".responsive-content");
 let closeResponsiveMenu = document.querySelector(".close-responsive-menu");
+let header = document.querySelector("header");
+let responsiveNavbar = document.querySelector(".responsive-navbar");
+let scrollUp = document.querySelector(".scroll-up-btn");
+
 openMenu.addEventListener("click", () => {
   sideBar.style.width = "540px";
   sideBar.style.padding = " 0 60px";
@@ -25,4 +29,26 @@ closeResponsiveMenu.addEventListener("click", () => {
   responsiveContent.style.display = "none";
   responsiveMenu.style.padding = "0";
   responsiveMenu.style.width = "0%";
+});
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    header.style.position = "fixed";
+    responsiveNavbar.style.position = "fixed";
+    header.style.backgroundColor = "#F4DBD7";
+    scrollUp.style.display = "block";
+  } else {
+    header.style.position = "static";
+    responsiveNavbar.style.position = "static";
+    header.style.backgroundColor = " #fff";
+    scrollUp.style.display = "none";
+  }
+}
+window.addEventListener("scroll", function () {
+  scrollFunction();
+});
+scrollUp.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+
 });
