b4e71f0d8e3ceee57413391d5c433967986a6dbd
YaqutVerdiyeva <yaqutverdiyeva22@gmail.com>
2024-07-09 21:29:44 +0400

    add some changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,7 +32,7 @@ closeResponsiveMenu.addEventListener("click", () => {
 });
 
 function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+  if (document.documentElement.scrollTop > 80) {
     header.style.position = "fixed";
     responsiveNavbar.style.position = "fixed";
     header.style.backgroundColor = "#F4DBD7";
@@ -48,7 +48,5 @@ window.addEventListener("scroll", function () {
   scrollFunction();
 });
 scrollUp.addEventListener("click", () => {
-  document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
-
 });
